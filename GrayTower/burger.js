75167a95dd27b23d70e33b02e2d759b194507470
malyna2 <malynovskyiandriy35@gmail.com>
2020-06-20 15:33:48 +0300

    fvddf
    1hgrhrthrth45g g 5445g 45gg45g4 5

--- GrayTower/burger.js
+++
@@ -1,8 +0,0 @@
-function burger() {
-    var x = document.getElementById("navMenu-burger");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
