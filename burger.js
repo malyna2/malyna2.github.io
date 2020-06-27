0daf751cc6abbb5dd64555033f5e94ef4076e77d
Adeodonne <bbozhik284@gmail.com>
2020-06-27 14:43:31 +0300

    add header

--- burger.js
+++ burger.js
@@ -1,8 +1,22 @@
-function burger() {
+document.getElementById("navMenu-burger").style.right = "-100%";
+function burger() {  
     var x = document.getElementById("navMenu-burger");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+    x.style.transition = "1s"
+    if (x.style.right === "-100%") {
+      x.style.right = "0";
     } else {
-      x.style.display = "block";
+      x.style.right = "-100%";
     }
   }
+
+$(function() {
+  $(window).resize(resizeBurger);
+});
+
+ function resizeBurger(){
+  let widthWindow = document.documentElement.clientWidth;
+  let logo_width = document.getElementById("mobilelogo").offsetWidth;
+ let burger_width = widthWindow - logo_width;
+  $("#navMenu-burger").width(burger_width);
+ console.log(document.getElementById('navMenu-burger').style.width);
+}
